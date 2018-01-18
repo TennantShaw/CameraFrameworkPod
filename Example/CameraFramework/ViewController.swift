dd1ebdb8f82e5692589cee18dbd96bc65324759b
Tennant Shaw <ctshaw325@gmail.com>
2018-01-18 02:23:43 -0500

    troubles

--- Example/CameraFramework/ViewController.swift
+++ Example/CameraFramework/ViewController.swift
@@ -7,18 +7,43 @@
 //
 
 import UIKit
+import CameraFramework
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Framework Version: \(CameraViewController.getVersion()!)")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func startButtonTapped() {
+        let camera = CameraViewController.init()
+        camera.delegate = self
+        camera.position = .back
+        present(camera, animated: true, completion: nil)
+    }
+    
 }
 
+
+extension ViewController: CameraControllerDelagate {
+    func stillImageCaptured(controller: CameraViewController, image: UIImage) {
+        self.imageView.image = image
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func cancelButtonTapped(controller: CameraViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+}
